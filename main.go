4260833e59b10e5617034bcba58da9acf0848b5e
ZacharyGroff <groff.zackm@gmail.com>
2019-06-19 23:13:23 -0400

    update main with endpoint class

--- main.go
+++ main.go
@@ -1,24 +1,10 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-	"io/ioutil"
-)
-
-func webhookHandler(writer http.ResponseWriter, request *http.Request) {
-	body, _ := ioutil.ReadAll(request.Body)
-	fmt.Printf("Headers:\n%v\n\n", request.Header)
-	fmt.Printf("Body:\n%s\n", body)
-}
-
 func main() {
 	//accept as user input or read from file in future
-	port := 8080
-	portString := fmt.Sprintf(":%d", port)
-	log.Printf("Starting endpoint on port %d\n", port)
-	
-	http.HandleFunc("/webhook", webhookHandler)
-	log.Fatal(http.ListenAndServe(portString, nil))
+	port := uint16(8080)
+	route := "/webhook"
+
+	endpoint := Endpoint{port, route}
+	endpoint.Start()
 }
