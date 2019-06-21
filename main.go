fa096cdd788642b41587d740cfb8299a7477e1a0
ZacharyGroff <groff.zackm@gmail.com>
2019-06-21 13:04:54 -0400

    read config in Main

--- main.go
+++ main.go
@@ -1,10 +1,7 @@
 package main
 
 func main() {
-	//accept as user input or read from file in future
-	port := uint16(8080)
-	route := "/webhook"
-
-	endpoint := Endpoint{port, route}
+	configPtr := NewConfig() 
+	endpoint := Endpoint{configPtr}
 	endpoint.Start()
 }
