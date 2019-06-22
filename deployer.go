9a3435723e304955cfbd45a5daa8ca75244c8719
ZacharyGroff <groff.zackm@gmail.com>
2019-06-22 12:30:24 -0400

    initial attempt at auto deployer

---
+++ deployer.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strings"
+)
+
+type Deployer struct {
+	scriptPath string
+}
+
+func NewDeployer(scriptPath string) *Deployer {
+	return &Deployer{scriptPath} 
+}
+
+func (deployer Deployer) Deploy() Error {
+	out, err := os.exec.Command(deyployer.scriptPath).Output()
+	
+	if err != nil {
+		log.Fatal("Error while deploying")
+	}
+	
+	log.Printf("Successfully deployed script with output:\n%s\n", out)
+}
