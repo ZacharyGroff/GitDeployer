e56471168ed868c9c606e30b38cca76d3fc48f82
ZacharyGroff <groff.zackm@gmail.com>
2019-06-28 13:21:44 -0400

    create time struct and override json to handle differing json timestamp formats

---
+++ models/time.go
@@ -0,0 +1,36 @@
+package models
+
+import (
+	"time"
+	"bytes"
+	"strconv"
+)
+
+type Time struct {
+	time.Time
+}
+
+func (t *Time) MarshalJSON() ([]byte, error) {
+	return t.Time.MarshalJSON()
+}
+
+func (t *Time) UnmarshalJSON(data []byte) error {
+	if bytes.Compare(data, []byte("null")) == 0 {
+		t.Time = time.Time{}
+		return nil
+	}
+	
+	error := t.Time.UnmarshalJSON(data)
+	if error == nil {
+		return nil
+	}
+
+	trimmedBytes := string(bytes.Trim(data, `"`))
+	seconds, err := strconv.ParseInt(trimmedBytes, 10, 64)
+	if err != nil {
+		return err
+	}
+
+	t.Time = time.Unix(seconds, 0)
+	return nil
+}
